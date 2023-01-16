8431169e9a46d9c6e35475d10d9277039d5ee8a5
Сергей Банников <SBannikov@croc.ru>
2023-01-16 19:22:41 +0300

    Лекция 34 - попытка записи

--- Piano/Program.cs
+++ Piano/Program.cs
@@ -9,7 +9,7 @@ namespace Piano
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// Точка входа в приложение
         /// </summary>
         [STAThread]
         static void Main()
