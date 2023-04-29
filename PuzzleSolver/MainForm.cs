9bb59b471f28e607a3ab9f4578e47a1d624b50c8
Сергей Банников <SBannikov@croc.ru>
2023-04-29 23:36:05 +0300

    Создание нового приложения

---
+++ PuzzleSolver/MainForm.cs
@@ -0,0 +1,13 @@
+namespace PuzzleSolver
+{
+    /// <summary>
+    /// Главная форма
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
