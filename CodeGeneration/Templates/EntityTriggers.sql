a8b7c04e8d8fd1c38052e6944b63f7e7469dfe21
Сергей Банников <SBannikov@croc.ru>
2023-12-16 17:24:13 +0300

    Пример кодогенерации (часть 1)

---
+++ CodeGeneration/Templates/EntityTriggers.sql
@@ -0,0 +1,16 @@
+﻿--
+-- Создание функции для заполнения поля Modified 
+--
+CREATE OR REPLACE FUNCTION public.func_update_modified()
+RETURNS trigger
+LANGUAGE plpgsql
+AS $BODY$
+BEGIN
+  new."Modified" := LOCALTIMESTAMP;
+  RETURN NEW;
+END;
+$BODY$
+;
+--
+-- Создание триггеров для заполнения поля Modified 
+-- 
---
+++ CodeGeneration/Templates/EnumConverters.cs
@@ -0,0 +1,72 @@
+﻿// ******************************************************************//
+// Файл строится автоматически на основе перечислимых типов проекта  //
+// ******************************************************************//
+
+using CodeGeneration.Enums;
+
+namespace CodeGeneration.Serialization
+{
+    /// <summary>
+    /// Особая сериализация типа <seealso cref="AccessLevel"/>
+    /// </summary>
+    public class AccessLevelConverter : EnumConverter<AccessLevel>
+    {
+    }
+
+    /// <summary>
+    /// Особая сериализация типа <seealso cref="AuthenticationMethod"/>
+    /// </summary>
+    public class AuthenticationMethodConverter : EnumConverter<AuthenticationMethod>
+    {
+    }
+
+    /// <summary>
+    /// Особая сериализация типа <seealso cref="ColumnBrowseDataType"/>
+    /// </summary>
+    public class ColumnBrowseDataTypeConverter : EnumConverter<ColumnBrowseDataType>
+    {
+    }
+
+    /// <summary>
+    /// Особая сериализация типа <seealso cref="DateType"/>
+    /// </summary>
+    public class DateTypeConverter : EnumConverter<DateType>
+    {
+    }
+
+    /// <summary>
+    /// Особая сериализация типа <seealso cref="FeatureType"/>
+    /// </summary>
+    public class FeatureTypeConverter : EnumConverter<FeatureType>
+    {
+    }
+
+    /// <summary>
+    /// Особая сериализация типа <seealso cref="IconType"/>
+    /// </summary>
+    public class IconTypeConverter : EnumConverter<IconType>
+    {
+    }
+
+    /// <summary>
+    /// Особая сериализация типа <seealso cref="NotificationsStatus"/>
+    /// </summary>
+    public class NotificationsStatusConverter : EnumConverter<NotificationsStatus>
+    {
+    }
+
+    /// <summary>
+    /// Особая сериализация типа <seealso cref="NotificationType"/>
+    /// </summary>
+    public class NotificationTypeConverter : EnumConverter<NotificationType>
+    {
+    }
+
+    /// <summary>
+    /// Особая сериализация типа <seealso cref="StaffType"/>
+    /// </summary>
+    public class StaffTypeConverter : EnumConverter<StaffType>
+    {
+    }
+
+}
