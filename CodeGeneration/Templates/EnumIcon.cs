b958896257842d66540288d8abe17f45607bc96d
Сергей Банников <SBannikov@croc.ru>
2023-12-16 17:28:12 +0300

    Генерация списка значков

--- CodeGeneration/Templates/EnumIcon.cs
+++ CodeGeneration/Templates/EnumIcon.cs
@@ -1,1 +1,77 @@
-﻿
+﻿// ******************************************************************//
+// Файл строится автоматически на основе исходного файла TypeScript  //
+// Исходный файл: .\CodeGeneration\WebApp\index.tsx 
+// ******************************************************************//
+// Статус генерации: True 
+
+namespace CodeGeneration.Enums 
+{
+    /// <summary>
+    /// Значки интерфейса
+    /// </summary>
+    public enum EnumIcon
+    {    
+
+  arrow,
+  menuHome,
+  menuPlan, // x
+  menuFacts,
+  menuReport, // x
+  menuReject, // x
+  menuMasterData, // x
+  menuAdminSettings, // x
+  menuAbout,
+  menuGantt, // x
+  menuBar, // x
+  statusDone,
+  statusCross,
+  statusInProcess,
+  statusAttention,
+  actionsMain,
+  actionsLinks, // x
+  actionsDelete,
+  actionsPlus,
+  actionsPlus2,
+  actionsLook,
+  actionsEdit, // x
+  actionsMove,
+  tableDrag,
+  tableFilter,
+  largeProduction, // x
+  cip, // x
+  dropdown,
+  load,
+  timeSeparator,
+  excel,
+  text,
+  syncUser,
+  history,
+  allowMoveDown, // x
+  menuRecipes,
+  fileTextIcon,
+  inputEnterButton,
+  actionsDownload,
+  cursorIcon,
+  reloadIcon,
+  largeDataMatrixIcon, //x
+  largeAdmin, // x
+  spiner,
+  largeSpiner,
+  largeAps, // x
+  largeCh4, // x
+  lagreGTS, // x
+  menuInstrumentIcon, // x
+  menuMeasurementIcon, // x
+  menuMonitoringIcon, // x
+  menuMapIcon, // x
+  menuRemedyActionIcon, // x
+  MoveUpIcon,
+  MoveDownIcon,
+  MoveLeftIcon,
+  MoveRightIcon,
+  user,
+  time,
+  calendar,
+
+    }
+}
